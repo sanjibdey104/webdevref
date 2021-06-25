6f65a4d391d73ab7369b7a0920d51254c1d297a6
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-06-25 09:47:32 +0530

    fix: document meta description

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -5,7 +5,7 @@ const CustomHead = () => {
     return (
         <Head>
             <title>WevDevRef</title>
-            <meta name="webdevref" content="a resource reference blog for web developers" />
+            <meta name="description" content="a resource reference blog for web developers" />
             <link rel="icon" href="/favicon.ico" />
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
