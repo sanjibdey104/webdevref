259d95b5f2c7b92f5a0c91a8013de6ea4621e413
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-06-28 18:08:02 +0530

    feat(search): post search filter

--- pages/posts.js
+++ pages/posts.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PostCard from '../components/PostCard';
 import { getPosts } from '../lib/data';
+import { FiSearch } from "react-icons/fi";
 
 const AllPostsSection = styled.section`
   width: 90%;
@@ -11,10 +12,43 @@ const AllPostsSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 3rem;
 
   h2 {
     font-size: 1.5rem;
-    margin-bottom: 3rem;
+  }
+
+  .post-search {
+    margin-bottom: 2rem;
+    width: 20rem;
+    position: relative;
+
+    .search-bar {
+      width: 100%;
+      border: 0;
+      outline: 0;
+      border-radius: 1rem;
+      height: 1.85em;
+      padding: 0.2rem 1rem;
+
+      font-size: 1.1rem;
+      font-weight: 500;
+      color: ${({theme}) => theme.textColor};
+      /* border: 1px solid ${({theme}) => theme.accentColor}; */
+
+      background-color: ${({theme}) => theme.backgroundColor};
+      box-shadow: ${({theme}) => theme.boxShadow};
+    }
+
+    .search-icon {
+      position: absolute;
+      right: 0.5rem;
+      top: 50%;
+      transform: translateY(-50%);
+      color: ${({theme}) => theme.accentColor};
+      font-size: 1.2rem;
+      font-weight: 600;
+    }
   }
 
   .post-list {
@@ -37,13 +71,29 @@ const AllPostsSection = styled.section`
 const Posts = ({ postsData }) => {
 
   const {posts} = postsData;
+  const [searchValue, setSearchValue] = useState('');
+  
+  let filteredPosts = posts.filter(post => {
+    return post.title.toLowerCase().includes(searchValue.toLowerCase());
+  })
 
   return (
     <AllPostsSection>
       <h2>all the posts</h2>
 
+      <div className="post-search">
+        <input type="text" className="search-bar" 
+        placeholder="search for a post..."
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+        />
+        <button className="search-icon">
+        <FiSearch />
+        </button>
+      </div>
+
       <ul className="post-list">
-          {posts.map(post => {
+          {filteredPosts.map(post => {
           const { id } = post;
           return <PostCard key={id} {...post}/>
           })}     
