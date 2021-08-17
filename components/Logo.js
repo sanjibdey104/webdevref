4c3b2c90b93ebefb87769fc6a9ccf8e472ba18a0
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-08-17 08:51:18 +0530

    style: logo cursor pointer

--- components/Logo.js
+++ components/Logo.js
@@ -3,20 +3,20 @@ import styled from "styled-components";
 import Link from "next/link";
 
 const StyledLogo = styled.div`
-  font-size: 0.9rem;
-  font-weight: 700;
-  line-height: 1;
-
   display: grid;
   place-content: center;
+  cursor: pointer;
 
   width: 4.4rem;
   height: 4.4rem;
-  background-color: var(--logo-color);
-  color: black;
   padding: 0.5rem;
 
+  color: black;
+  font-size: 0.9rem;
+  font-weight: 700;
+  line-height: 1;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  background-color: var(--logo-color);
 `;
 
 const Logo = () => {
