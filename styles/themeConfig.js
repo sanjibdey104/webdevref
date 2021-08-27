6865a4cf57195c9f1034baa115b85e8f741e6b58
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-08-27 16:21:24 +0530

    refactor: first load with light theme as default

--- styles/themeConfig.js
+++ styles/themeConfig.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const ThemePreference = () => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("light");
 
   const setThemePreference = (mode) => {
     window.localStorage.setItem("themePref", mode);
