38c6e84f30c2458563c193b8fc5513d9a6203de3
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-08-27 16:18:22 +0530

    fix: flashing barebone html

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,6 @@ import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -13,6 +12,7 @@ export default class MyDocument extends Document {
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
+      const initialProps = await Document.getInitialProps(ctx);
 
       return {
         ...initialProps,
