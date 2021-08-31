b39aac6b41b0e2ee8864e698b2a2916a539a6bb6
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-08-31 12:50:26 +0530

    update: about section content

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const AboutSection = styled.section`
-  width: 65%;
+  width: 60%;
   margin: 0 auto;
   margin-bottom: 1rem;
+
+  @media (max-width: 600px) {
+    width: 100%;
+  }
 
   h2 {
     font-family: var(--font-secondary);
@@ -12,25 +16,15 @@ const AboutSection = styled.section`
     margin-bottom: 2rem;
   }
 
-  .body {
-    display: flex;
-  }
-
   p {
     margin-bottom: 1rem;
   }
 
-  @media (max-width: 600px) {
-    width: 100%;
-
-    h2 {
-      text-align: center;
-      font-size: 1.3rem;
-    }
-    .body {
-      display: flex;
-      flex-direction: column;
-    }
+  blockquote {
+    margin: 2rem 0;
+    padding: 0.85rem;
+    border-left: 5px solid ${({ theme }) => theme.accentColor};
+    background-color: ${({ theme }) => theme.accentColorLighter};
   }
 `;
 
@@ -39,28 +33,29 @@ const About = () => {
     <AboutSection>
       <h2>motivation behind the blog</h2>
 
-      <div className="body">
-        <div className="content">
-          <p>
-            For a beginner, self-taught web developer the overwhelming aspect of
-            the learning curve, even before starting out with actually learning
-            the technologies is the huge assortment of technologies to start
-            with.
-          </p>
-          <p>
-            Being one such developer myself, I've had my fair share of confusion
-            and exhaustion.{" "}
-          </p>
-          <p>
-            So, I started out with documentations and blog posts for a better
-            clarity of the topics I was getting exposed to and to maintained a
-            reference through chrome bookmarks.
-          </p>
-          <p>
-            Through this blog, I want to share all such resources to the
-            community out there.
-          </p>
-        </div>
+      <div className="about">
+        <p>
+          For a beginner, self-taught web developer the overwhelming aspect of
+          the learning curve is the huge assortment of technologies to start
+          with. Being one such developer myself, I've had my fair share of
+          eagerness and confusion. But, that is part of the process.
+        </p>
+        <blockquote>
+          Learning by sharing is the gospel of honest growth.
+        </blockquote>
+        <p>
+          The goal was to have a space to help keep track of my learnings,
+          document it and maybe help others white I'm at it.
+        </p>
+        <p>
+          Building the blog from scratch, not only taught me about new tech and
+          tools, but serves as as decent side project, plus as an added bonus,
+          logging and revisiting self drafted posts serves as a sweet revision
+          tactic as well - to brush up and retain concepts.
+        </p>
+        <p>
+          This is my token of contribution to the developer community out there.
+        </p>
       </div>
     </AboutSection>
   );
