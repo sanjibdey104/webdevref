3337271ced14972fd723fc5ad8829d09b2cf515c
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-08-31 16:41:38 +0530

    refactor: google font preconnect crossorigin attribute

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -14,7 +14,7 @@ const CustomHead = () => {
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
-        crossOrigin="true"
+        crossOrigin="anonymous"
       />
       <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
