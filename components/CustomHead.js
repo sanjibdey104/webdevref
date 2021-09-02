a3c5105e88faf7a070c0792506cb32ec0f748081
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-09-02 18:26:30 +0530

    update: favicons folder

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -9,23 +9,23 @@ const CustomHead = () => {
         name="description"
         content="a resource reference blog for web developers"
       />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicons/favicon.ico" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="/favicons/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/favicons/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/favicons/favicon-16x16.png"
       />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
