518789c8232d60eb3e0f96c855aa8fd66d0c4097
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-09-02 18:24:54 +0530

    update favicons

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -10,6 +10,23 @@ const CustomHead = () => {
         content="a resource reference blog for web developers"
       />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         rel="preconnect"
